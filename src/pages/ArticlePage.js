554251bd807831825defea6f88e5cfda87b96d51
Yong Chiang Pin <cpyong1996@gmail.com>
2021-04-07 01:11:36 +0800

    comment list and upvotes section

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    //called whenever url change, the name
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const result = await fetch(`/article/${name}`);
+    //         const body = await result.json();
+    //         setArticleInfo(body);
+    //     };
+    // }, [name]);
 
     if (!article) return <NotFoundPage />;
 
@@ -14,9 +27,11 @@ const ArticlePage = ({ match }) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
             <h3>Other Articles:</h3>
             <ArticlesList articles={otherArticles} />
         </>
